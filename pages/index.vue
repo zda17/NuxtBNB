259cb974a3e5b25c1dc022cb86130dea87a9e51e
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2022-01-16 11:05:30 -0600

    setup project, add homes.json, create first component

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="home in homes" :key="home.objectID" style="float: left; margin: 10px;">
+            <home-card :home="home"></home-card>
+        </div>
+    </div>
+</template>
+<script>
+import homes from '~/data/homes';
+import HomeCard from '../components/HomeCard.vue';
+
+export default {
+  components: { HomeCard },
+    data() {
+        return {
+            homes: homes.slice(0,3),
+        }
+    },
+}
+</script>
