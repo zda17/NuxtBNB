e42efad5ed3d281c57302ecf8a675405a04801e7
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2022-01-16 11:41:45 -0600

    add individual home images, title

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,12 +1,31 @@
 <template>
     <div>
+        <div style="display: flex">
+            <img v-for="image in home.images" :key="image" :src="image"  alt="image of home...." width="200" height="150">
+        </div>
         {{ home.title }}
+        <br>
+        ${{ home.pricePerNight }} / night 
+        <br>
+        <img src="/images/marker.svg" width="20" height="20" alt="location icon">
+        {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}
+        <br>
+        <img src="/images/star.svg" width="20" height="20" alt="location icon">
+        {{ home.reviewValue }}
+        <br>
+        {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath
+        <br>
     </div>
 </template>
 <script>
 import homes from '~/data/homes'
 
 export default {
+    head() {
+        return {
+            title: this.home.title
+        }
+    },
     data() {
         return {
             home: {},
