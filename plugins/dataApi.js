d5b728ab310838275ab74bb13e37a39cefb211ad
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2022-01-17 14:52:05 -0600

    handle errors

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,19 +1,41 @@
 export default function(context, inject) {
     const appId = 'KT2YR8EGCP';
     const apiKey = 'd08a437382fd487110c93248e41e1306';
+    const headers = {
+        'X-Algolia-API-Key': apiKey,
+        'X-Algolia-Application-Id': appId
+    }
 
     inject('dataApi', {
         getHome
     })
 
     async function getHome(homeId) {
-        const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, {
-            headers: {
-                'X-Algolia-API-Key': apiKey,
-                'X-Algolia-Application-Id': appId
-            }
-        })
+        try {
+            return unwrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, { headers }))
+        } catch(error) {
+            return getErrorResponse(error);
+        }
+    }
+
+    async function unwrap(response) {
         const json = await response.json();
-        return json;
+        const { ok, status, statusText } = response;
+        return {
+            json,
+            ok,
+            status,
+            statusText,
+        }
+    }
+
+    // always remember to write for errors / edge cases, not just best case scenario
+    function getErrorResponse(error) {
+        return {
+            ok: false,
+            status: 500,
+            statusText: error.message,
+            json: {}
+        }
     }
 }
