d09fe378dae125bda60eee452407d37a8f48c6a1
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2022-01-17 09:07:25 -0600

    isolate/abstract google maps API into nuxt plugin

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -30,15 +30,6 @@ export default {
     head() {
         return {
             title: this.home.title,
-            script: [{
-                src: "https://maps.googleapis.com/maps/api/js?key=api_key&Libraries=places&callback=initMap",
-                hid: "map",
-                defer: true,
-                skip: process.client && window.mapLoaded
-            }, {
-                innerHTML: "window.initMap = function() { window.mapLoaded = true }",
-                hid: "map-init",
-            }],
         }
     },
     data() {
@@ -46,27 +37,9 @@ export default {
             home: {},
         }
     },
-    methods: {
-        showMap() {
-            const mapOptions = {
-                zoom: 18,
-                center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng),
-                disableDefaultUI: true,
-                zoomControl: true,
-            };
-            const map = new window.google.maps.Map(this.$refs.map, mapOptions);
-            const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng);
-            const marker = new window.google.maps.Marker({ position });
-            marker.setMap(map);
-        }
-    },
+    methods: {},
     mounted() {
-        const timer = setInterval(() => {
-            if (window.mapLoaded) {
-                clearInterval(timer);
-                this.showMap();
-            }
-        }, 200);
+        this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng);
     },
     created() {
         const home = homes.find((home) => home.objectID == this.$route.params.id); 
